eb59d07341585f0370c7017ddf5ee4b3f37c27c7
tgxzd <aizaddtengkuazman@gmail.com>
2024-09-20 17:22:51 +0800

    Refactor message controller and models
    - Refactor the sendMessage function in the message controller to improve code readability and organization.
    - Update the conversation and message models to include timestamps for createdAt and updatedAt.
    - Add a new getMessages function in the message controller to retrieve messages for a specific user.

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import { sendMessage } from '../controllers/message.controller.js';
+import { getMessages } from '../controllers/message.controller.js';
 import protectRoute from '../middleware/protectRoute.js';
 const router = express.Router();
 
 
+router.get('/:id' , protectRoute,getMessages);
 router.post('/send/:id' , protectRoute,sendMessage);
+
+
 
 
 
